c32b2f3ec2bbca77d66728b3eed53cad1ee7de7d
Kristof <kristjank@pm.me>
2024-06-28 09:21:26 +0200

    chore: initial testing

---
+++ Makefile
@@ -0,0 +1,20 @@
+# include .env file and export its env vars
+# (-include to ignore error if it does not exist)
+-include .env
+
+# deps
+install:; forge install
+update:; forge update
+
+# Build & test
+build  :; forge build
+test   :; forge test -vv --fork-url $MAINNET_RPC_URL --fork-block-number $BLOCK_NUMBER
+trace   :; forge test -vvv --fork-url $MAINNET_RPC_URL --fork-block-number $BLOCK_NUMBER
+clean  :; forge clean
+snapshot :; forge snapshot
+fmt    :; forge fmt && forge fmt test/
+
+
+# Deploy
+deploy-sepolia:; forge script -vv script/SepoliaEth/Deployer.s.sol:DeployerScript --rpc-url ${SEPOLIA_RPC_URL} --private-key ${DEPLOY_KEY} —optimize —optimizer-runs 200 --with-gas-price 45000000000 --verify --etherscan-api-key ${ETHERSCAN_KEY} --slow --broadcast
+#deploy-sepolia-registry:; forge script script/SepoliaEth/Deployer.s.sol:RegistryScript --rpc-url ${SEPOLIA_RPC_URL} --private-key ${DEPLOY_KEY} —optimize —optimizer-runs 200 --with-gas-price 25000000000 --verify --etherscan-api-key ${ETHERSCAN_KEY} --slow --broadcast
---
+++ script/SepoliaEth/Deployer.s.sol
@@ -0,0 +1,45 @@
+pragma solidity 0.8.21;
+
+import {Deployer} from "src/Deployer.sol";
+import {Registry} from "src/Registry.sol";
+
+import "forge-std/Script.sol";
+
+/**
+ * @dev Run
+ *      `source .env && forge script script/testnet/Deployer.s.sol:DeployerScript --rpc-url $TESTNET_RPC_URL  --private-key $DEPLOYER_DEPLOYER_KEY —optimize —optimizer-runs 200 --with-gas-price 25000000000 --verify --etherscan-api-key $ETHERSCAN_KEY --slow --broadcast`
+ *      `source .env && forge script script/testnet/Sepolia.s.sol:DeployerScript --rpc-url $TESTNET_RPC_URL  --private-key $DEPLOYER_KEY —optimize —optimizer-runs 200 --with-gas-price 25000000000 --verify --etherscan-api-key $ETHERSCAN_KEY --slow --broadcast`
+ * @dev Optionally can change `--with-gas-price` to something more reasonable
+ */
+contract DeployerScript is Script {
+    Deployer public deployer;
+    Registry public registry;
+    uint256 deployerPrivateKey;
+
+    //These addresses are can be the same. One it the owner address for the Deployer contract. The other address is permissioned to use the deployer contract for other deployments. They can be the same on testnet.
+    address public sommDev = 0xd1ed25240ecfa47fD2d46D34584c91935c89546c;
+    address public sommDeployerOwner = 0xd1ed25240ecfa47fD2d46D34584c91935c89546c;
+
+    function setUp() external {
+        deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    }
+
+    function run() external {
+        address[] memory deployers = new address[](1);
+        deployers[0] = sommDev;
+
+        bytes memory creationCode;
+        bytes memory constructorArgs;
+
+        vm.startBroadcast();
+
+        deployer = new Deployer(sommDeployerOwner, deployers);
+        deployer.transferOwnership(sommDev);
+
+        creationCode = type(Registry).creationCode;
+        constructorArgs = abi.encode(sommDev, sommDev, address(0), address(0));
+        registry = Registry(deployer.deployContract("THE-REGISTER", creationCode, constructorArgs, 0));
+
+        vm.stopBroadcast();
+    }
+}
