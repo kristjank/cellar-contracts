7581397daf919654943d5b3237539457f8d7f88b
Kristof <kristjank@pm.me>
2024-06-28 10:55:24 +0200

    chore: price router deploy

--- Makefile
+++ Makefile
@@ -15,6 +15,10 @@ snapshot :; forge snapshot
 fmt    :; forge fmt && forge fmt test/
 
 
-# Deploy
+# Deploy SEPOLIA
+# deployer and registry
 deploy-sepolia:; forge script -vv script/SepoliaEth/Deployer.s.sol:DeployerScript --rpc-url ${SEPOLIA_RPC_URL} --private-key ${DEPLOY_KEY} —optimize —optimizer-runs 200 --with-gas-price 70000000000 --verify --etherscan-api-key ${ETHERSCAN_KEY} --slow --broadcast
 # deploy-sepolia-registry:; forge script -vv script/SepoliaEth/Deployer.s.sol:RegistryScript --rpc-url ${SEPOLIA_RPC_URL} --private-key ${DEPLOY_KEY} —optimize —optimizer-runs 200 --with-gas-price 70000000000 --verify --etherscan-api-key ${ETHERSCAN_KEY} --slow --broadcast
+
+# price router setup
+deploy-sepolia-price-router:; forge script -vv script/SepoliaEth/DeployPriceRouter.s.sol:DeployPriceRouterScript --rpc-url ${SEPOLIA_RPC_URL} --private-key ${DEPLOY_KEY} —optimize —optimizer-runs 200 --with-gas-price 70000000000 --verify --etherscan-api-key ${ETHERSCAN_KEY} --slow --broadcast
---
+++ script/SepoliaEth/DeployPriceRouter.s.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.21;
+
+import {Deployer} from "src/Deployer.sol";
+import {Registry} from "src/Registry.sol";
+import {PriceRouter} from "src/modules/price-router/PriceRouter.sol";
+
+import {PositionIds} from "resources/PositionIds.sol";
+import {Math} from "src/utils/Math.sol";
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+
+import "forge-std/Script.sol";
+import "forge-std/StdJson.sol";
+
+/**
+ *  source .env && forge script script/Arbitrum/production/DeployPriceRouter.s.sol:DeployPriceRouterScript --evm-version london --with-gas-price 100000000 --slow --broadcast --etherscan-api-key $ARBISCAN_KEY --verify
+ * @dev Optionally can change `--with-gas-price` to something more reasonable
+ */
+contract DeployPriceRouterScript is Script {
+    using Math for uint256;
+    using stdJson for string;
+
+    uint256 public privateKey;
+    Deployer public deployer = Deployer(vm.envAddress("SEPOLIA_DEPLOYER_ADDRESS"));
+    Registry public registry;
+    PriceRouter public priceRouter;
+
+    uint8 public constant CHAINLINK_DERIVATIVE = 1;
+    uint8 public constant TWAP_DERIVATIVE = 2;
+    uint8 public constant EXTENSION_DERIVATIVE = 3;
+    ERC20 public WETH = ERC20(0xD0dF82dE051244f04BfF3A8bB1f62E1cD39eED92);
+
+    function setUp() external {
+        privateKey = vm.envUint("PRIVATE_KEY");
+        registry = Registry(deployer.getAddress("THE-REGISTER"));
+    }
+
+    function run() external {
+        bytes memory creationCode;
+        bytes memory constructorArgs;
+        vm.startBroadcast(privateKey);
+
+        // Deploy Price Router
+        creationCode = type(PriceRouter).creationCode;
+        constructorArgs = abi.encode(vm.envAddress("SOMM_DEVELOPER_ADDRESS"), registry, WETH);
+        priceRouter = PriceRouter(deployer.deployContract("THE-REGISTER-ROUTER", creationCode, constructorArgs, 0));
+
+        // Update price router in registry.
+        registry.setAddress(2, address(priceRouter));
+
+        priceRouter.transferOwnership(vm.envAddress("OWNER_ADDRESS"));
+
+        vm.stopBroadcast();
+    }
+}
